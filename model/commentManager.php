e1c004ce773a0c1cee2d03bb53ff301e3346c6a9
TristanLax <Tanamassar@gmail.com>
2018-09-24 21:04:08 +0200

    Correction d'une erreur empêchant l'incrémentation correcte des reports

--- model/commentManager.php
+++ model/commentManager.php
@@ -27,6 +27,6 @@ class CommentManager extends PDOFactory
     public function reportComment($id) 
     {
         $db = $this->dbConnect();
-        $report = $db->exec('UPDATE comments SET reports=+1 WHERE id ='.(int) $id);
+        $report = $db->exec('UPDATE comments SET reports = reports +1 WHERE id ='.(int) $id);
     }
 }
